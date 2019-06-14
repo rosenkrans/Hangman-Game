bd9755fd03afd62b0b819a0b00595b519d940b40
rosenkrans <mark.rosenkrans@gmail.com>
2019-06-14 14:39:47 -0400

    worked on event listener in alphabet for loop

--- main.js
+++ main.js
@@ -9,29 +9,64 @@ var word = "BANANA";
 //convert word into an array so letters can be counted and referenced
 var wordArray = word.split("");
 console.log(wordArray) 
-
+//create a div for each letter in the word and add same class and dynamic value
 for(let i=0; i<wordArray.length; i++){
     var blank = document.createElement('div');
     // blank.id = 'each-letter' + [i];
     blank.className = 'letter';
-    blank.setAttribute('value', wordArray[i]);
+    blank.dataset.letter = wordArray[i];
     document.getElementById('letters').appendChild(blank)
-    
 }
-
 
 //Create alphabet string and convert to array
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
 var alphArray = alphabet.split("");
 console.log(alphArray) 
-
+//create a button for each alphabet letter and add class and value and visible letter
 for(let i=0; i<alphArray.length; i++){
     var alphButtons = document.createElement('button');
     alphButtons.className = 'alph-buttons';
-    alphButtons.setAttribute('value', alphArray[i]); 
-    alphButtons.innerText = alphButtons.getAttribute('value')
+    alphButtons.dataset.letter = alphArray[i]; 
+    alphButtons.innerText = alphArray[i];
+    alphButtons.addEventListener('click', function(evt){
+        if(wordArray.includes(evt.target.dataset.letter)){
+            evt.target.style.backgroundColor = 'green';
+            //get all divs with data-letter that = alphabet dataset.letter
+        }
+        
+    })
+
     document.getElementById('alphabetID').appendChild(alphButtons) 
+    
 }
+
+// for(let i=0; i<alphArray.length; i++){
+//     alphButtons.addEventListener('click', function(evt){
+//         var alphCompare = document.querySelector('alph-buttons') 
+//         //not sure if I need this
+//         var wordCompare = document.querySelectorAll('letter')
+
+//         alphCompare.forEach('value'){
+//             if(//value of alph-buttons[i] === value of letter[i])
+//             {
+//                 evt.target.style.backgroundColor = 'green';
+//                 //disable alphabet button
+//                 //populate the letter in the div
+//             }
+//             //Else if the letter is not in the word, change the button to red 
+//             //and reduce the 5/5 t 4/5 and so on...
+//         }
+//     }   
+// }
+
+
+
+
+
+
+
+
+
 
 
 
